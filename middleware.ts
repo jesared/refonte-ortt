cc48577a4ac0a3dcb35487680b7da24f4a84c9c6
jerome jesared <jesared@gmail.com>
2026-03-15 12:38:10 +0100

    up middle

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export async function middleware(req: NextRequest) {
 
   const token = await getToken({
     req,
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
   });
 
   if (!token) {
